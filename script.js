c43853529baf21ad010af2d1ce61e2473911e8c7
Emmanuel Balikuddembe <balixemma@gmail.com>
2022-09-10 09:56:21 +0300

    Identify bug

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 function toggleClasses() {
-<<<<<<< HEAD
+
     const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
     mobileMenu.classList.toggle();
 }
-=======
-  const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
-  mobileMenu.classList.toggle('open');
-}
 
->>>>>>> 1394e9675d39e3250a52dcd7c83b8a31c88a2af2
+  const mobileMenu = document.getElementsByClassName('mobile-menu')[0];
+  mobileMenu.classList.toggle();
+
 toggleClasses();
 toggleClasses();
